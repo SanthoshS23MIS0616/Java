7ebac3c0d901bcc47ce0fd9ad80205e3f70900aa
santhosh2720 <your-real-github-email@gmail.com>
2026-05-28 22:55:43 +0530

    Complete Day 7 OOP basics tasks

--- Phase1_Java/Day07_OOPBasics/Tasks/Rectangle.java
+++ Phase1_Java/Day07_OOPBasics/Tasks/Rectangle.java
@@ -2,3 +2,39 @@
 Question:
 How can you create a Java program to Create width and height fields, area(), perimeter(), and a static totalRectangles counter shared by all objects?
 */
+
+import java.util.Scanner;
+class Rectangle{
+    private int length;
+    private int breadth;
+    Rectangle(int length, int breadth){
+        this.length=length;
+        this.breadth=breadth;
+
+    }
+    public void area(){
+        int area_rect = length*breadth;
+        System.out.println("the are of rectanle is :" + area_rect);
+    }
+    public void perimeter(){
+        int peri_rect = (2*length)+(2*breadth);
+        System.out.println("the are of rectanle is :" + peri_rect);
+    }
+}
+class ArrayBasics{
+    public static void main (String args[])
+    {
+       
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("enter the length: ");
+        int length=sc.nextInt();
+        System.out.println("enter the breadth: ");
+        int breadth=sc.nextInt();
+        Rectangle r = new Rectangle(length, breadth);
+        r.area();
+        r.perimeter();
+       
+
+    }
+}
--- Phase1_Java/Day07_OOPBasics/Tasks/Student.java
+++ Phase1_Java/Day07_OOPBasics/Tasks/Student.java
@@ -2,3 +2,88 @@
 Question:
 How can you create a Java program to Create private fields name, rollNo, cgpa, and branch, add constructor, getters, setters, toString(), and isEligibleForPlacement() where cgpa >= 7.5?
 */
+
+import java.util.Scanner;
+class Info{
+    private String roll;
+    private String name;
+    private float cgpa;
+    private String branch;
+    private int m1,m2,m3;
+
+    Info(String roll,String name,String branch,float cgpa,int m1, int m2, int m3)
+    {
+        this.roll = roll;
+        this.name= name;
+        this.branch = branch;
+        this.cgpa = cgpa;
+        this.m1 = m1;
+        this.m2 = m2;
+        this.m3 = m3;
+    }
+    public int total()
+    {
+        int total1 = m1+m2+m3;
+        System.out.println("the total of the 3 sub is : " +total1);
+        return total1;
+        
+    }
+    public float avg()
+    {
+        int totalmark = total();
+        float avg1 = totalmark/3;
+        System.out.println("the avrg of 3 sub is:" + avg1);
+        return avg1;
+    }
+    public void grades()
+    {
+        float grade = avg();
+        if(grade > 80){
+            System.out.println("A grade");
+        }
+        else if (grade >50 && grade<79){
+            System.out.println("B grade ");
+
+        }
+        else {
+            System.out.println("C grade");
+        }
+    }
+    public  void details()
+    {
+
+        System.out.println( "Rollno:" + roll +"\n" +"Name:" +name+ "\n" + "course" + branch +"\n");
+    }
+    }
+  
+
+
+class Student{
+    public static void main (String args [])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the rollno:");
+        String roll = sc.next();
+        System.out.println ("ente the name: ");
+        String name = sc.next();
+
+        System.out.println("branch : ");
+        String branch = sc.next();
+
+        System.out.println("enter cgpa: ");
+        float cgpa = sc.nextFloat();
+        System.out.println("enter the marks m1,m2,m3:");
+        
+        int m1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int m3 = sc.nextInt();
+        Info data = new Info(roll,name,branch,cgpa,m1,m2,m3);
+        
+        
+        data.details();
+        data.grades();
+        
+
+
+    }
+}
