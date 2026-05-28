f57853aa6c145f2d9180e19d1e619b6d05f64185
santhosh2720 <your-real-github-email@gmail.com>
2026-05-28 22:55:55 +0530

    Complete Day 8 constructor chaining task

--- Phase1_Java/Day08_InheritancePolymorphism/Tasks/ConstructorChainTest.java
+++ Phase1_Java/Day08_InheritancePolymorphism/Tasks/ConstructorChainTest.java
@@ -2,3 +2,50 @@
 Question:
 How can you demonstrate constructor chaining in Java using a Person class and an Employee class that calls super(name) first?
 */
+
+
+import java.util.Scanner;
+
+
+class Person{
+    String name;
+    int age ;
+    Person(String name,int age)
+    {
+        this.name = name;
+        this.age= age;
+        System.out.println("Name:" + name);
+        System.out.println(" age: " +age);
+    }
+    
+}
+ class Employee extends Person{
+    int salary;
+    String job;
+    Employee(String name,int salary,String job,int age)
+    {
+        super(name,age);
+        this.salary = salary;
+        this.job = job;
+       
+    }
+    void display(){
+         System.out.println("the salary of" + name + "is :" + salary + " and the job is :" +job);
+    }
+}
+
+public class ConstructorChainTest{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the name:");
+        String name = sc.next();
+        System.out.println("Enter the age: ");
+        int age = sc.nextInt();
+        System.out.println("Enter the salary: ");
+        int salary = sc.nextInt();
+        System.out.println("Enter the job: ");
+        String job = sc.next();
+        Employee e = new Employee(name,salary,job,age);
+        e.display();
+    }
+}
