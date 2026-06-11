215c7f10dbf89a88ac23996ead7de29a06b241ff
santhosh2720 <your-real-github-email@gmail.com>
2026-06-11 23:58:35 +0530

    Add LinkedList demo for queue and stack operations

--- Phase1_Java/Day10_CollectionsList/Tasks/LinkedListDemo.java
+++ Phase1_Java/Day10_CollectionsList/Tasks/LinkedListDemo.java
@@ -2,3 +2,29 @@
 Question:
 How can you create a Java program to use LinkedList as a queue with addLast/removeFirst and as a stack with push/pop?
 */
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+
+    public static void main(String[] args) {
+
+        LinkedList<Integer> queue = new LinkedList<>();
+
+        queue.addLast(10);
+        queue.addLast(20);
+        queue.addLast(30);
+
+        System.out.println("Queue Remove: " + queue.removeFirst());
+        System.out.println("Queue: " + queue);
+
+        LinkedList<Integer> stack = new LinkedList<>();
+
+        stack.push(100);
+        stack.push(200);
+        stack.push(300);
+
+        System.out.println("Stack Pop: " + stack.pop());
+        System.out.println("Stack: " + stack);
+    }
+}
