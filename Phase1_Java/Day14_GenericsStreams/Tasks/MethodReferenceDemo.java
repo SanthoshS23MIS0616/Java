2de66d0f8ac6ecc37f90f4fb67bd544918f786d3
santhosh2720 <your-real-github-email@gmail.com>
2026-06-11 23:45:06 +0530

    Add method reference examples using println and toUpperCase

--- Phase1_Java/Day14_GenericsStreams/Tasks/MethodReferenceDemo.java
+++ Phase1_Java/Day14_GenericsStreams/Tasks/MethodReferenceDemo.java
@@ -2,3 +2,20 @@
 Question:
 How can you replace lambda expressions with method references such as System.out::println and String::toUpperCase in Java?
 */
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MethodReferenceDemo {
+
+    public static void main(String[] args) {
+
+        List<String> names = Arrays.asList("java", "python", "c++");
+
+        names.forEach(System.out::println);
+
+        names.stream()
+             .map(String::toUpperCase)
+             .forEach(System.out::println);
+    }
+}
