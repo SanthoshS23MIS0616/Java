d521fd116e0009d0580122a2951d81cd99884c6d
santhosh2720 <your-real-github-email@gmail.com>
2026-06-11 23:46:35 +0530

    Add Comparable and Comparator based student sorting demo

--- Phase1_Java/Day14_GenericsStreams/Tasks/SortingDemo.java
+++ Phase1_Java/Day14_GenericsStreams/Tasks/SortingDemo.java
@@ -2,3 +2,62 @@
 Question:
 How can you create a Java program to sort a List<Student> in multiple ways: by name, by cgpa descending, by cgpa then name, using Comparable and Comparator?
 */
+
+import java.util.*;
+
+class Student implements Comparable<Student> {
+    private String name;
+    private double cgpa;
+
+    public Student(String name, double cgpa) {
+        this.name = name;
+        this.cgpa = cgpa;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCgpa() {
+        return cgpa;
+    }
+
+    @Override
+    public int compareTo(Student other) {
+        return this.name.compareTo(other.name);
+    }
+
+    @Override
+    public String toString() {
+        return name + " - " + cgpa;
+    }
+}
+
+public class SortingDemo {
+
+    public static void main(String[] args) {
+
+        List<Student> students = new ArrayList<>();
+
+        students.add(new Student("Santhosh", 8.5));
+        students.add(new Student("Arun", 9.1));
+        students.add(new Student("Bala", 8.5));
+        students.add(new Student("Kavin", 7.9));
+
+        Collections.sort(students);
+        System.out.println("Sort by Name:");
+        System.out.println(students);
+
+        students.sort(Comparator.comparingDouble(Student::getCgpa).reversed());
+        System.out.println("Sort by CGPA Descending:");
+        System.out.println(students);
+
+        students.sort(
+                Comparator.comparingDouble(Student::getCgpa)
+                          .reversed()
+                          .thenComparing(Student::getName)
+        );
+        System.out.println("Sort by CGPA Then Name:");
+        System.out.println(students);
+    }
+}
