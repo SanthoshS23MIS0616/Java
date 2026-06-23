4635133fd356dfaaeea145c679374b687ccb38b6
SanthoshS23MIS0616 <santhoshs23mis0616@gmail.com>
2026-06-23 23:29:09 +0530

    Update GetMiddleDemo.java

--- Phase2_DSA/Day15_SinglyLinkedList/Tasks/GetMiddleDemo.java
+++ Phase2_DSA/Day15_SinglyLinkedList/Tasks/GetMiddleDemo.java
@@ -2,3 +2,38 @@
 Question:
 How can you find the middle node of a singly linked list in one pass using slow and fast pointers in Java?
 */
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+public class GetMiddleDemo {
+
+    static ListNode getMiddle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        ListNode middle = getMiddle(head);
+
+        System.out.println("Middle Node: " + middle.val);
+    }
+}
