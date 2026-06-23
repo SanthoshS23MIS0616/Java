d3189b23263c3043a25a840b6f7a35d54b28b7b5
SanthoshS23MIS0616 <santhoshs23mis0616@gmail.com>
2026-06-23 23:27:10 +0530

    Update FloydDryRun.java

--- Phase2_DSA/Day16_LinkedListPatterns/Tasks/FloydDryRun.java
+++ Phase2_DSA/Day16_LinkedListPatterns/Tasks/FloydDryRun.java
@@ -2,3 +2,48 @@
 Question:
 How does Floyd’s cycle detection algorithm move slow and fast pointers through a cyclic linked list until they meet?
 */
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+public class FloydDryRun {
+
+    public static boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;          // 1 step
+            fast = fast.next.next;     // 2 steps
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(3);
+        ListNode n4 = new ListNode(4);
+        ListNode n5 = new ListNode(5);
+
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+        n5.next = n3; // cycle
+
+        System.out.println(hasCycle(n1)); // true
+    }
+}
